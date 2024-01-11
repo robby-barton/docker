91697f09e4069ab72ef220cfeb886157c8963812
Robby Barton <rbartonsc@gmail.com>
2024-01-10 20:05:52 -0500

    Container updates (#6)
    * switch to plex; add anime sonarr instance; add recyclarr; switch to protonvpn
    
    * update readme

--- servarr_users.sh
+++ servarr_users.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash
+
 sudo groupadd mediacenter -g 13000
-sudo useradd -r jellyfin -u 13000 -g mediacenter
+sudo useradd -r jellyfin -u 13000 -G mediacenter,video,render
 sudo useradd -r sonarr -u 13001 -g mediacenter
 sudo useradd -r radarr -u 13002 -g mediacenter
 sudo useradd -r bazarr -u 13003 -g mediacenter
 sudo useradd -r sabnzbd -u 13004 -g mediacenter
 sudo useradd -r qbittorrent -u 13005 -g mediacenter
 sudo useradd -r prowlarr -u 13006 -g mediacenter
+sudo useradd -r animarr -u 13007 -g mediacenter
